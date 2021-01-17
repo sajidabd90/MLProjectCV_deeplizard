2ad3d967318c065b7b4b08e971e151bbd6a27aa4
Abdullah Al Sajid <sajidabd90@gmail.com>
2021-01-17 21:00:27 +0600

    dummy data generated and preprocessed

--- keras_deeplizard_data.py
+++ keras_deeplizard_data.py
@@ -26,8 +26,18 @@ for i in range(50):
 for i in range(1000):
     random_younger = randint(13,64)
     train_samples.append(random_younger)
-    train_samples.append(0)
+    train_labels.append(0)
 
     random_older = randint(65,100)
     train_samples.append(random_older)
     train_labels.append(1)
+
+#Convert them to numpy arrays 
+train_samples = np.array(train_samples)
+train_labels = np.array(train_labels)
+train_labels, train_samples = shuffle(train_labels, train_samples)
+
+#Scale data from 0 to 1
+scaler = MinMaxScaler(feature_range= (0,1))
+scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1,1))
+
