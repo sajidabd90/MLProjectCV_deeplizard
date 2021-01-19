d69aea157c8d8a158b11098d8fb0c09806070991
Abdullah Al Sajid <sajidabd90@gmail.com>
2021-01-19 23:11:01 +0600

    Removed test file

--- .vscode/test1.py
+++
@@ -1,9 +0,0 @@
-# Building a boilerplate for a Keras Sequential Model
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-
-
-layers = [
-    Dense(units = 3, input_shape=(2,), activation='relu',),
-    Dense(units =2 , activation= 'softmax')
-]
